b024791bdb304c287062a362cd076e882075752e
Agbugba David C <57929456+Davidruph@users.noreply.github.com>
2023-08-28 15:46:13 +0100

    login page

--- step.js
+++ step.js
@@ -101,8 +101,198 @@ $(document).ready(function () {
     $(".progress-bar").css("width", percent + "%");
   }
 
-  $(".submit").click(function () {
-    // return false;
-    window.location.replace("3.html");
+  toggleSubmitButton();
+
+  $(".otp-input").keyup(function (e) {
+    if (
+      (e.keyCode >= 48 && e.keyCode <= 57) ||
+      (e.keyCode >= 96 && e.keyCode <= 105)
+    ) {
+      // If the key is a number, move to the next input
+      $(this).next(".otp-input").focus();
+    } else if (e.keyCode == 8) {
+      // On pressing backspace, move to the previous input
+      $(this).prev(".otp-input").focus();
+    }
+
+    // Check to enable/disable the submit button
+    toggleSubmitButton();
+  });
+
+  function toggleSubmitButton() {
+    let isAllFilled = true;
+
+    $(".otp-input").each(function () {
+      if ($(this).val() === "") {
+        isAllFilled = false;
+        return false;
+      }
+    });
+
+    if (isAllFilled) {
+      $(".submit").removeAttr("disabled");
+    } else {
+      $(".submit").attr("disabled", "disabled");
+    }
+  }
+
+  $("#school_name").on("keyup", function () {
+    var school_name = $(this).val().toLowerCase().trim(); // trim() will remove leading/trailing spaces
+
+    if (!school_name) {
+      $("#handle").attr("placeholder", "Handle (school abbreviation)");
+      return; // Exit the function if school_name is empty
+    }
+
+    let acronym = school_name
+      .split(/\s+/)
+      .reduce((response, word) => (response += word.slice(0, 1)), "");
+
+    $("#message").text("Handle Suggestion : " + acronym);
+    $("#handle").attr("placeholder", acronym + " (Please type your handle)");
+    $("#username").val("admin@" + acronym);
+    $("#message_container").show();
+  });
+
+  $("#email").on("blur", function () {
+    var email = $(this).val();
+    var firstname = $("#firstname").val();
+    var lastname = $("#lastname").val();
+    var school_name = $("#school_name").val();
+
+    $.ajax({
+      type: "POST",
+      url: "{{ route('sendOtp') }}",
+      headers: {
+        "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+      },
+      data: {
+        email: email,
+        firstname: firstname,
+        lastname: lastname,
+        school_name: school_name,
+      },
+      success: function (response) {
+        // Store the OTP in local storage
+        toastr.success("An OTP has been sent to your email.");
+        localStorage.setItem("otp", response.otp);
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        console.log("Error: " + errorThrown);
+      },
+    });
+  });
+
+  $(".btn-last-step").on("click", function () {
+    var email = $("email").val();
+    var firstname = $("#firstname").val();
+    var lastname = $("#lastname").val();
+    var school_name = $("#school_name").val();
+
+    $.ajax({
+      type: "POST",
+      url: "{{ route('sendOtp') }}",
+      headers: {
+        "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+      },
+      data: {
+        email: email,
+        firstname: firstname,
+        lastname: lastname,
+        school_name: school_name,
+      },
+      success: function (response) {
+        // Store the OTP in local storage
+        toastr.success("An OTP has been sent to your email.");
+        localStorage.setItem("otp", response.otp);
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        console.log("Error: " + errorThrown);
+      },
+    });
+  });
+
+  $(".submit").on("click", function (e) {
+    e.preventDefault();
+
+    var school_name = $("#school_name").val();
+    var handle = $("#handle").val();
+    var country = $("#country").val();
+    var school_website = $("#school_website").val();
+    var firstname = $("#firstname").val();
+    var lastname = $("#lastname").val();
+    var gender = $("#gender").val();
+    var email = $("#email").val();
+    var school_phone = $("#school_phone").val();
+    var username = $("#username").val();
+    var password = $("#password").val();
+    var role_in_school = "admin";
+    var otp =
+      $(".otp-input:eq(0)").val() +
+      $(".otp-input:eq(1)").val() +
+      $(".otp-input:eq(2)").val() +
+      $(".otp-input:eq(3)").val() +
+      $(".otp-input:eq(4)").val();
+
+    // Get the OTP value entered by the user
+    let userEnteredOtp =
+      $(".otp-input:eq(0)").val() +
+      $(".otp-input:eq(1)").val() +
+      $(".otp-input:eq(2)").val() +
+      $(".otp-input:eq(3)").val() +
+      $(".otp-input:eq(4)").val();
+
+    // Compare with stored OTP
+    let storedOtp = localStorage.getItem("otp");
+
+    if (userEnteredOtp === storedOtp) {
+      $.ajax({
+        type: "POST",
+        url: "{{ route('sign_up.store') }}",
+        data: {
+          school_name: school_name,
+          handle: handle,
+          country: country,
+          school_website: school_website,
+          first_name: firstname,
+          last_name: lastname,
+          gender: gender,
+          email: email,
+          mobile_number: school_phone,
+          username: username,
+          password: password,
+          role_in_school: role_in_school,
+          otp: otp,
+        },
+
+        headers: {
+          "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+        },
+        success: function (response) {
+          if (response.message === "success") {
+            toastr.success(
+              "Your account was created successfully! Redirecting to login..."
+            );
+
+            // Redirects after 3 seconds (3000 milliseconds)
+            setTimeout(function () {
+              window.location.href = "/onboarding-success";
+            }, 3000);
+          } else {
+            toastr.error(
+              "Error. Something went wrong, unable to create account."
+            );
+          }
+        },
+
+        error: function (jqXHR, textStatus, errorThrown) {
+          toastr.error(
+            "Error. Something went wrong, unable to create account. Try again or contact system administrator."
+          );
+        },
+      });
+    } else {
+      toastr.error("Incorrect OTP. Please try again.");
+    }
   });
 });
